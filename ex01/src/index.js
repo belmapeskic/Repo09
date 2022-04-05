b4c4096e6bad9f11ace25358aecbe787e2653ea1
belmapeskic <belma.peskic@gmail.com>
2022-04-06 00:06:34 +0200

    Adding file index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function myTrueBoolean() {
+    var boolean = true;
+    boolean = true;
+    return true/false;
+}
+// Only change code above this line
+
+console.log((true));
+module.exports = myTrueBoolean;
