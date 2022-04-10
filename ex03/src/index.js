7769532c7df001869a23327a818a26d4ce67e4c6
belmapeskic <belma.peskic@gmail.com>
2022-04-10 12:17:51 +0200

    Adding file index.js

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,21 +1,20 @@
 // Only change code below this line
 function myEqualFunction(num) {
-    if (num) { 
+    if (num === 23) { 
         return "Equal";
-    }
+    } 
     return "Not equal";
 }
-myEqualFunction(5 == 23); // false
-myEqualFunction(23 == 23); // true
-myEqualFunction("23" === 23); // false
-myEqualFunction('23' === 23); // false
-myEqualFunction("text" == 23); // false
+myEqualFunction(5 === 23); // Not equal
+myEqualFunction(23 === 23); // Equal
+myEqualFunction("23"=== 23); // Not equal
+myEqualFunction('23' === 23); // Not equal
+myEqualFunction("text" === 23); // Not equal
 // Only change code above this line
 
-console.log(myEqualFunction(false));
-console.log(myEqualFunction(true));
-console.log(myEqualFunction(false));
-console.log(myEqualFunction(false));
-console.log(myEqualFunction(false));
-
+console.log(myEqualFunction(5)); 
+console.log(myEqualFunction(23)); 
+console.log(myEqualFunction("23")); 
+console.log(myEqualFunction('23')); 
+console.log(myEqualFunction("text")); 
 module.exports = myEqualFunction;
